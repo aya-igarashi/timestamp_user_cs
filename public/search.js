3de68299ebd993338637b3848349bd097b85ea4f
Glitch (hello-express) <none>
2021-08-26 20:46:59 +0000

    💂☂️ Checkpoint ./views/search.html:35147739/67 ./public/search.js:35147739/273 ./server.js:35147739/316

--- public/search.js
+++ public/search.js
@@ -2,7 +2,7 @@ document.getElementById("toform").onclick = function(){
   // ログイン処理
   
   // 成功した場合はページ遷移
-  location.href = "/index";
+  location.href = "/toform";
 };
 
 document.getElementById("tosearch").onclick = function(){
