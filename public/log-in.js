02006f0d099295584ebfbe1c7323fc60d0e9d5f1
Glitch (oxidized-universal-cast) <none>
2021-11-17 13:38:46 +0000

    🐫💰 Checkpoint ./package.json:35147739/270 ./server.js:35147739/2523 ./public/log-in.js:35147739/758

--- public/log-in.js
+++ public/log-in.js
@@ -5,27 +5,3 @@ document.getElementById("login").onclick = function(){
 document.getElementById("newlogin").onclick = function(){
   
 };
-
-// ログイン処理
-  // Get the username and password from the UI.
-  var username = document.getElementById("username-field").value;
-  var password = document.getElementById("password-field").value;
-
-  // Create a KiiUser object.
-  var user = KiiUser.userWithUsername(username, password);
-  // Register the user asynchronously.
-  user.register({
-    // If the user was registered
-    success: function(theUser) {
-      console.log("User registered: " + JSON.stringify(theUser));
-
-      // Go to the main screen.
-      location.href = "/input";
-    },
-    // If the user was not registered
-    failure: function(theUser, errorString) {
-      alert("Unable to register user: " + errorString);
-      console.log("Unable to register user: " + errorString);
-    }
-  });
-
